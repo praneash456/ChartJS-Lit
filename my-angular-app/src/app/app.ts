1682169c920739106cf070a3f963751a3a908228
praneash <gppraneash@gmail.com>
2025-06-15 21:19:07 +0530

    added chartjs to lit

--- my-angular-app/src/app/app.ts
+++ my-angular-app/src/app/app.ts
@@ -1,15 +1,127 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
-import '../../../dist/my-lit-lib/my-lit-lib.js'
+import chartDataLabels from 'chartjs-plugin-datalabels';
+import '../../../dist/my-lit-lib/my-lit-lib.js';
+import { Legend, plugins } from 'chart.js';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class App {
-  protected title = 'my-angular-app';
+  type = 'bar';
+  datalabels = [chartDataLabels];
+  labels = ['Category 1', 'Category 2', 'Category 3'];
+
+  data = {
+    labels: this.labels,
+    datasets: [
+      {
+        label: 'temperature',
+        data: [35, 30, 25],
+        backgroundColor: '#0066FF',
+        borderRadius: 20,
+        pointStyle: 'circle',
+      },
+      {
+        label: 'pressure',
+        data: [30, 22, 15],
+        backgroundColor: '#1ac69e',
+        borderRadius: 20,
+        pointStyle: 'circle',
+      },
+      {
+        label: 'wind',
+        data: [34, 22, 25],
+        backgroundColor: '#2708a2',
+        borderRadius: 20,
+        pointStyle: 'circle',
+      },
+      {
+        label: 'humidity',
+        data: [36, 15, 37],
+        backgroundColor: '#0046b0',
+        borderRadius: 20,
+        pointStyle: 'circle',
+      },
+      {
+        label: 'precipitation',
+        data: [34, 22, 27],
+        backgroundColor: '#078878',
+        borderRadius: 20,
+        pointStyle: 'circle',
+      },
+      {
+        label: 'precipitation',
+        data: [36, 10, 30],
+        backgroundColor: '#2708a2',
+        borderRadius: 20,
+        pointStyle: 'circle',
+      },
+    ],
+  };
+
+  options = {
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        labels: {
+          usePointStyle: true,
+        },
+        title: {
+          display: true,
+          text: 'legend',
+        },
+        position: 'right',
+      },
+      tooltip: {
+        usePointStyle: true,
+      },
+      datalabels: {
+        color: '#000',
+        anchor: 'end',
+        align: 'top',
+        offset: 4,
+        font: {
+          weight: 'bold',
+          size: 12,
+        },
+        formatter: (value: any) => {
+          return value;
+        },
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: 'categories',
+          font: {
+            size: 14,
+            weight: 'bold',
+          },
+          padding: {
+            top: 10,
+          },
+        },
+      },
+      y: {
+        beginAtZero : true,
+        title : {
+          display : true,
+          text: 'Fuel Burned (Gal)',
+          font : {
+            size: 14,
+            weight: 'bold'
+          }
+        }
+      }
+    },
+  };
 }
--- my-lit-lib/src/lib/my-lit-lib.ts
+++ my-lit-lib/src/lib/my-lit-lib.ts
@@ -1,16 +1,126 @@
-import { LitElement, html, css } from 'lit';
+import {
+  LitElement,
+  html,
+  css,
+  TemplateResult,
+  PropertyValueMap,
+  unsafeCSS,
+  PropertyValues,
+} from 'lit';
+import { Chart, ChartConfiguration, registerables } from 'chart.js';
+import { property } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+Chart.register(...registerables);
+Chart.defaults.font.size = 14;
+Chart.defaults.font.family = 'Noto Sans, sans-serif';
+
+import styles from  './my-lit-lib.scss?inline'
 
 export class MyElement extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-      padding: 16px;
-      color: var(--my-element-text-color, black);
-    }
-  `;
 
-  render() {
-    return html`<h2>Hello from Lit!</h2>`;
+  static styles = unsafeCSS(styles)
+  @property({ type: Object })
+  chart: Chart | undefined;
+
+  @property({ type: String })
+  type:
+    | 'line'
+    | 'bar'
+    | 'scatter'
+    | 'bubble'
+    | 'pie'
+    | 'doughnut'
+    | 'polarArea'
+    | 'radar'
+    | undefined;
+
+  @property({ type: Object })
+  data: unknown | undefined;
+
+  @property({ type: Object })
+  options: unknown | undefined;
+
+  @property({ type: Array })
+  plugins: [] | undefined;
+
+  @property({ type: Object })
+  arialabel: unknown | undefined;
+
+  @property({ type: Object })
+  longDesc: unknown | undefined;
+
+  private config:
+    | {
+        type:
+          | 'line'
+          | 'bar'
+          | 'scatter'
+          | 'bubble'
+          | 'pie'
+          | 'doughnut'
+          | 'polarArea'
+          | 'radar'
+          | undefined;
+
+        data: unknown | undefined;
+        options: unknown | undefined;
+        plugins: [] | undefined;
+      }
+    | undefined;
+
+  protected firstUpdated(_changedProperties: PropertyValues): void {
+    const data = this.data || {};
+    const options = this.options || {};
+    const plugins = this.plugins || [];
+    this.config = {
+      type: this.type,
+      data: data,
+      options: options,
+      plugins: plugins,
+    };
+    if (!this.chart) {
+      const ctx: CanvasRenderingContext2D | null | undefined = this.shadowRoot
+        ?.querySelector('canvas')
+        ?.getContext('2d');
+      if (ctx) {
+        this.chart = new Chart(
+          ctx,
+          this.config as ChartConfiguration<any, any, any>
+        );
+      }
+    }
+  }
+
+  protected updated(
+    changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>
+  ): void {
+    if (
+      changedProperties.has('type') ||
+      changedProperties.has('data') ||
+      changedProperties.has('options') ||
+      changedProperties.has('plugins')
+    ) {
+      if (this.config) {
+        this.config.type = this.type;
+        this.config.data = this.data;
+        this.config.options = this.options;
+        this.config.plugins = this.plugins;
+      }
+      this.chart?.update();
+    }
+  }
+
+  render(): TemplateResult<1> {
+    return html`
+      <div part="base" class="canvas__wrapper">
+        <canvas
+          class="chart__canvas"
+          aria-label=${ifDefined(this.arialabel)}
+          longdesc=${ifDefined(this.longDesc)}
+          role="img"
+        ></canvas>
+      </div>
+    `;
   }
 }
 
